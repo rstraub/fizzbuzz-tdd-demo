a27005c1f6ba904a273df304423203511f255448
Roy Straub <roy@codecraftr.nl>
2023-07-17 13:27:44 +0200

    Rudamentary setup

--- src/main/scala/nl/codecraftr/scala/kata/Main.scala
+++ src/main/scala/nl/codecraftr/scala/kata/Main.scala
@@ -1,5 +1,13 @@
 package nl.codecraftr.scala.kata
 
 object Main extends App {
-  println("hello, world")
+    private def fizzBuzz = {
+        (1 to 100)
+            .map(transform)
+            .foreach(println)
+    }
+
+    private def transform(number: Int) = number.toString
+
+    fizzBuzz
 }
